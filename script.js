ebc012813a59759c3485337e8fc107c4fffb7e5f
danillkifly <danillkifly@gmail.com>
2024-11-29 14:50:10 +0700

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+let splash = document.querySelector(".splash");
+let logo = document.querySelector(".logo-header");
+let logoSpan = document.querySelectorAll(".logo-intro");
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    logoSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 400);
+    });
+
+    setTimeout(() => {
+      logoSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (idx + 1) * 50);
+      });
+    }, 2000);
+
+    setTimeout(() => {
+      splash.style.top = "-100vh";
+    }, 2300);
+  });
+});
+
 const bars = document.querySelector(".bars");
 const navbar = document.querySelector(".navbar");
 const xmark = document.querySelector(".xmark");
